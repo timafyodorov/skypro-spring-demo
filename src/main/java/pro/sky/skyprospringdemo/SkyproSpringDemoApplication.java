dcefde5a39748c34d7d40348b67ebbb7c1056654
home <timurfedorov042@gmail.com>
2025-05-26 16:32:45 +0300

    HM-start-spring

--- src/main/java/pro/sky/skyprospringdemo/SkyproSpringDemoApplication.java
+++ src/main/java/pro/sky/skyprospringdemo/SkyproSpringDemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkyproSpringDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkyproSpringDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproSpringDemoApplication.class, args);
+    }
 
 }
